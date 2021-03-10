552ce135932b7d624bd5f68cce88102a5b1e0c2a
4ooper <79720254+4ooper@users.noreply.github.com>
2021-03-10 18:40:45 +0300

    init : start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(array.length > 0)
+    {
+    let min = 0;
+    for(let i=0;i<array.length;i++)
+        {
+            if(array[min]<array[i])
+                min = i;
+            return min;
+        }
+    }
+    else
+        return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array.length > 0)
+    {
+    let max = 0;
+    for(let i=0;i<array.length;i++)
+        {
+            if(array[max]>array[i])
+                max = i;
+            return max;
+        }
+    }
+    else
+        return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(array.length > 0)
+    {
+        let avg = 0;
+        for(let i=0;i<array.length;i++)
+            avg+=array[i]
+        avg = avg/array.length;
+        return avg;
+    }
+   else
+        return 0;
 }
