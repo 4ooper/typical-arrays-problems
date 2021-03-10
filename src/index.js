0d72947ab6d47da953ab6288ae7ae3c0285ecb34
4ooper <79720254+4ooper@users.noreply.github.com>
2021-03-10 20:01:24 +0300

    feat : add new algoritms

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
-
 exports.min = function min (array) {
-    return Math.min.apply(null, this);
+    if(array)
+        {
+            let min = 0;
+            for(let i = 0;i<array.length;i++)
+                {
+                    if(array[min]>array[i])
+                        min = i;
+                }
+            return array[min];
+        }
+    else
+        return 0;
 }
 
 exports.max = function max (array) {
-  return Math.max.apply(null, this);
+    if(array)
+        {
+            let max = 0;
+            for(let i = 0;i<array.length;i++)
+                {
+                    if(array[max]<array[i])
+                        max = i;
+                }
+            return array[max];
+        }
+    else
+        return 0;
 }
 
 exports.avg = function avg (array) {
-    if(array.length > 0)
+    if(array)
     {
         let avg = 0;
         for(let i=0;i<array.length;i++)
